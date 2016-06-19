2c1bebddb5d720638ffdf394adfcd5cb97cdb093
Mark Anderson <mark@m3b.net>
2016-06-19 22:15:11 +0200

    removed python3 support until scrapyd catches up

--- scrapy_feedexporter_azure_blob/__init__.py
+++ scrapy_feedexporter_azure_blob/__init__.py
@@ -1,18 +1,28 @@
 from scrapy.extensions.feedexport import BlockingFeedStorage
 from azure.storage.blob import BlockBlobService
 
-from urllib.parse import urlparse
-
-
 class AzureBlobFeedStorage(BlockingFeedStorage):
 
     def __init__(self, uri):
-        from scrapy.conf import settings
-        self.account_name = settings['AZURE_ACCOUNT_NAME']
-        self.account_key = settings['AZURE_ACCOUNT_KEY']
-        self.container = settings['AZURE_CONTAINER']
 
-        self.filename = settings['AZURE_FILENAME']
+        '''
+        Azure uses slashes '/' in their keys, which confuses the shit out of urlparse.
+        So, we just handle it ourselves here.
+        assuming format looks like this:
+
+        azure://account_name:password@container/filename.jsonl
+
+        azure://bobsaccount:1234567890abc1KUj0lK1gXHv4NHrCfKxfxHy3bwQJ+LqFHCay6r1S/Yhw2Ot4Tk6p1zF9IiMcPBo7o9poXZgA==@sites/filename.jsonl
+        '''
+
+        container = uri.split('@')[1].split('/')[0]
+        filename = '/'.join(uri.split('@')[1].split('/')[1::])
+        account_name, account_key = uri[8::].split('@')[0].split(':')
+
+        self.account_name = account_name
+        self.account_key = account_key
+        self.container = container
+        self.filename = filename
         self.blob_service = BlockBlobService(account_name=self.account_name, account_key=self.account_key)
 
     def _store_in_thread(self, file):
